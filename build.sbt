07a591cc96452cfc54b4ada5a93b3e86aec4931e
Agafontsev.Ivan <e1ywka@gmail.com>
2015-07-25 14:46:01 +0300

    Support POST with multipart/form-data

--- build.sbt
+++ build.sbt
@@ -8,4 +8,6 @@ resolvers += "spray repo" at "http://repo.spray.io"
 
 libraryDependencies += "io.spray" %% "spray-can" % "1.3.3"
 
+libraryDependencies += "io.spray" %% "spray-httpx" % "1.3.3"
+
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
--- src/main/scala/ru/infotecs/edi/service/RequestHandler.scala
+++ src/main/scala/ru/infotecs/edi/service/RequestHandler.scala
@@ -3,11 +3,14 @@
  */
 package ru.infotecs.edi.service
 
+import java.io.{ByteArrayInputStream, InputStreamReader, BufferedReader, StringReader}
+
 import akka.actor.Actor
 import akka.actor.Actor.Receive
 import spray.can.Http
 import spray.http._
 import HttpMethods._
+import spray.httpx.unmarshalling.{FormDataUnmarshallers, Unmarshaller}
 
 import scala.collection.mutable
 
@@ -23,8 +26,90 @@ class RequestHandler extends Actor {
       sender ! HttpResponse(status = 200, entity = "Server is working")
     }
 
-    case HttpRequest(POST, Uri.Path("/upload"), headers, HttpEntity.NonEmpty(ContentType(MediaTypes.`multipart/form-data`, None), data), _) => {
+    case HttpRequest(GET, Uri.Path("/upload"), _, _, _) => {
+      sender ! HttpResponse(status = 200, entity = HttpEntity(ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`), formUpload))
+    }
 
+    case HttpRequest(POST, Uri.Path("/upload"), headers, entity, _) => {
+      import FormDataUnmarshallers._
+
+      Unmarshaller.unmarshal[MultipartFormData](entity) match {
+        case Left(e) => sender ! HttpResponse(status = 500, "Unsupported media type")
+        case Right(data) => {
+          val fileUpload = data.get("file").foreach(body => println(body.entity.asString))
+        }
+      }
+
+      //println(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data.toByteArray), charset.getOrElse(HttpCharsets.`UTF-8`).nioCharset)))
+      sender ! HttpResponse(status = 200, "Ok")
     }
   }
+
+  lazy val formUpload =
+    """
+      |<html>
+      |<head>
+      |  <title>Plupload - spray</title>
+      |  <script src="http://www.plupload.com/plupload/js/plupload.full.min.js"></script>
+      |</head>
+      |<body>
+      |  <div id="filelist">Your browser doesn't have Flash, Silverlight or HTML5 support.</div>
+      |<br />
+      |
+      |<div id="container">
+      |    <a id="pickfiles" href="javascript:;">[Select files]</a>
+      |    <a id="uploadfiles" href="javascript:;">[Upload files]</a>
+      |</div>
+      |
+      |<br />
+      |<pre id="console"></pre>
+      |
+      |
+      |<script type="text/javascript">
+      |// Custom example logic
+      |
+      |var uploader = new plupload.Uploader({
+      |    runtimes : 'html5,html4',
+      |
+      |    browse_button : 'pickfiles', // you can pass in id...
+      |    container: document.getElementById('container'), // ... or DOM Element itself
+      |
+      |    url : "/upload",
+      |
+      |    filters : {
+      |        max_file_size : '10mb'
+      |    },
+      |
+      |    init: {
+      |        PostInit: function() {
+      |            document.getElementById('filelist').innerHTML = '';
+      |
+      |            document.getElementById('uploadfiles').onclick = function() {
+      |                uploader.start();
+      |                return false;
+      |            };
+      |        },
+      |
+      |        FilesAdded: function(up, files) {
+      |            plupload.each(files, function(file) {
+      |                document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
+      |            });
+      |        },
+      |
+      |        UploadProgress: function(up, file) {
+      |            document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
+      |        },
+      |
+      |        Error: function(up, err) {
+      |            document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+      |        }
+      |    }
+      |});
+      |
+      |uploader.init();
+      |
+      |</script>
+      |</body>
+      |</html>
+    """.stripMargin
 }
