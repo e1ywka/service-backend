4faaaf254b3d8ce6fedb9bd83a2a5850883d3dd8
Agafontsev.Ivan <e1ywka@gmail.com>
2015-08-04 23:33:07 +0300

    Test BufferingFileHandler respect chunk ordering

--- src/test/java/ru/infotecs/edi/service/FileHandlerSpec.scala
+++ src/test/java/ru/infotecs/edi/service/FileHandlerSpec.scala
@@ -4,11 +4,11 @@
 package ru.infotecs.edi.service
 
 import akka.actor.{ActorRef, Props, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit, TestActorRef}
+import akka.testkit.{TestProbe, ImplicitSender, TestKit, TestActorRef}
 import akka.pattern.ask
-import akka.util.Timeout
+import akka.util.{ByteString, Timeout}
 import org.scalatest._
-import ru.infotecs.edi.service.FileHandler.Init
+import ru.infotecs.edi.service.FileHandler.{FlushTo, Init}
 import ru.infotecs.edi.service.FileUploading.{UploadFinished, FileChunkUploaded, FileChunk}
 import spray.http.BodyPart
 
@@ -22,11 +22,12 @@ class FileHandlerSpec(_system: ActorSystem) extends TestKit(_system)
 
   implicit val timeout = Timeout(1 second)
 
-  override def afterAll {
+  override def afterAll() {
     TestKit.shutdownActorSystem(system)
   }
 
   "BufferingFileHandler" must {
+
     "become handling on Init message" in {
       val totalChunks = 2
       val actorRef = system.actorOf(Props.create(classOf[BufferingFileHandler], self))
@@ -40,12 +41,22 @@ class FileHandlerSpec(_system: ActorSystem) extends TestKit(_system)
       val actorRef = system.actorOf(Props.create(classOf[BufferingFileHandler], self))
       actorRef ! Init(totalChunks)
       actorRef ! FileChunk((0, totalChunks), BodyPart("<entity></entity>", "file"), "fileName")
-      expectMsg(FileChunkUploaded)
-      expectMsg(UploadFinished("fileName", true))
+      expectMsgAllOf(FileChunkUploaded, UploadFinished("fileName", true))
     }
 
     "respect ordering of chunks" in {
+      val totalChunks = 2
+      val actorRef = system.actorOf(Props.create(classOf[BufferingFileHandler], self))
+      actorRef ! Init(totalChunks)
+      actorRef ! FileChunk((1, totalChunks), BodyPart("</entity>", "file"), "fileName")
+      actorRef ! FileChunk((0, totalChunks), BodyPart("<entity>", "file"), "fileName")
+      expectMsgAllOf(FileChunkUploaded, FileChunkUploaded, UploadFinished("fileName", true))
 
+      val probe = TestProbe()
+      actorRef ! FlushTo(probe.ref)
+      probe.expectMsgPF() {
+        case b: ByteString => b.decodeString("UTF-8") should be("<entity></entity>")
+      }
     }
   }
 
