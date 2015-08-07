e902861f07031fa6514cbc053ddfb0d7796ee7b7
Иван Агафонцев <ivan.agafontsev@infotecs.ru>
2015-08-07 14:23:42 +0300

    Parsing of xml files

--- src/main/scala/ru/infotecs/edi/service/Parser.scala
+++ src/main/scala/ru/infotecs/edi/service/Parser.scala
@@ -3,18 +3,30 @@
  */
 package ru.infotecs.edi.service
 
-import akka.actor.{ActorRef, Actor}
+import akka.actor.Actor
 import akka.util.ByteString
-import ru.infotecs.edi.service.Parser.{Parsing, ValidXml}
+import ru.infotecs.edi.service.Parser.{InvalidXml, Parsing, ParsingResult, ValidXml}
+import ru.infotecs.edi.xml.documents.XMLDocumentReader
+import ru.infotecs.edi.xml.documents.clientDocuments.ClientDocument
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object Parser {
-  case object Parsing
-  case object ValidXml
-  case class InvalidXml(message: String)
+
+  trait ParsingResult
+
+  case object Parsing extends ParsingResult
+
+  case object ValidXml extends ParsingResult
+
+  case class InvalidXml(message: String) extends ParsingResult
+
 }
 
+/**
+ * Actor performs parsing xml documents and checks its validity.
+ */
 class Parser extends Actor {
 
   implicit val ec = context.dispatcher
@@ -22,20 +34,27 @@ class Parser extends Actor {
   var file: ByteString = _
 
   def receive: Receive = {
-    case f: ByteString => {
-      file = f
-      Future {
-        println(file.decodeString("UTF-8"))
-      } recoverWith {
-        case e: Exception => println(e.getMessage); Future.failed(e)
-      } foreach (_ => {
-        context become {
-          case _ => sender ! ValidXml
-        }
-      })
-      context become {
-        case _ => sender ! Parsing
+    case f: ByteString => Future {
+      val xmlDocument = XMLDocumentReader.read(f.iterator.asInputStream)
+      if (!xmlDocument.isInstanceOf[ClientDocument]) {
+        throw new Exception("Not a client document")
       }
-    }
+      xmlDocument
+    } andThen {
+      case Success(doc: ClientDocument) => doc.write(Console.out)
+      case Failure(e) => println(e.getMessage)
+    } recoverWith {
+      case e: Exception => {
+        context become parsingResult(InvalidXml(e.getMessage))
+        Future.failed(e)
+      }
+    } foreach (_ => {
+      context become parsingResult(ValidXml)
+    })
+      context become parsingResult(Parsing)
+  }
+
+  def parsingResult(parsingResult: ParsingResult): Receive = {
+    case _ => parsingResult
   }
 }
